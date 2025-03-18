886f293ef470166644f475a41aaf724e15a19088
Divyam Kumar Pandey <divyamkumarp@gmail.com>
2025-03-19 00:57:23 +0530

    welcome page and sidebar

---
+++ assets/assets.ts
@@ -0,0 +1,27 @@
+import logo_text from "./logo_text.svg";
+import logo_icon from "./logo_icon.svg";
+import menu_icon from "./menu_icon.svg";
+import chat_icon from "./chat_icon.svg";
+import sidebar_icon from "./sidebar_icon.svg";
+import sidebar_close_icon from "./sidebar_close_icon.svg";
+import chat_icon_dull from "./chat_icon_dull.svg";
+import phone_icon from "./phone_icon.svg";
+import phone_icon_dull from "./phone_icon_dull.svg";
+import qrcode from "./qrcode.png";
+import new_icon from "./new_icon.svg";
+import profile_icon from "./profile_icon.svg";
+
+export const assets = {
+  logo_text,
+  logo_icon,
+  menu_icon,
+  chat_icon,
+  sidebar_icon,
+  sidebar_close_icon,
+  chat_icon_dull,
+  phone_icon,
+  phone_icon_dull,
+  qrcode,
+  new_icon,
+  profile_icon,
+};
