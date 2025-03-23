296e6f7e21175b6bf10a2147a39318244d7b3ddf
Divyam Kumar Pandey <divyamkumarp@gmail.com>
2025-03-23 16:26:13 +0530

    fixed typo

--- config/db.ts
+++ config/db.ts
@@ -5,10 +5,10 @@ let cached = (global as any).mongoose || {conn: null, promise: null};
 export default async function connectDB(){
     if(cached.conn) return cached.conn;
     if(!cached.promise){
-        if(!process.env.MONGODB_URI) {
-            throw new Error("MONGODB_URI is not defined");
+        if(!process.env.MONGO_URI) {
+            throw new Error("MONGO_URI is not defined");
         }
-        cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) => mongoose);
+        cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => mongoose);
     }
     try {
         cached.conn = await cached.promise;
