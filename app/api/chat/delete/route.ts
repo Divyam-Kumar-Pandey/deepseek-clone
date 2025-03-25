ef35fd9cbb5d7b66ac6c9805bf4290010dd36278
Divyam Kumar Pandey <divyamkumarp@gmail.com>
2025-03-25 12:26:18 +0530

    chat model and related apis

---
+++ app/api/chat/delete/route.ts
@@ -0,0 +1,29 @@
+import connectDB from "@/config/db";
+import Chat from "@/models/Chat";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest){
+    try {
+     const { userId } = getAuth(req);
+     const { chatId } = await req.json();
+
+     
+    if (!userId) {
+      return NextResponse.json({
+        success: false,
+        message: "User not authenticated",
+      });
+    }
+
+    // Connect to the database and delete the chat
+
+    await connectDB();
+    await Chat.deleteOne({_id: chatId, userId})
+
+    return NextResponse.json({ success: true, message: "Chat Deleted" });
+
+    } catch (error) {
+        return NextResponse.json({ success: false, error: error instanceof Error ? error.message : "An unknown error occurred" }, { status: 500 });
+    }
+}
