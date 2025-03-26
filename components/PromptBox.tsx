5633a2e6999a75004dcf34be9969831fa4a23544
Divyam Kumar Pandey <divyamkumarp@gmail.com>
2025-03-26 14:31:06 +0530

    added axios, openai, and react-hot-toast dependencies; integrated toast notifications in PromptBox and AppContext; enhanced chat functionality with user prompts and message handling; updated layout with Toaster component for notifications

--- components/PromptBox.tsx
+++ components/PromptBox.tsx
@@ -1,16 +1,106 @@
 import { assets } from '@/assets/assets'
+import { useAppContext } from '@/context/AppContext';
+import axios from 'axios';
 import Image from 'next/image'
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
 
 
 const PromptBox = ({setIsLoading, isLoading}: {setIsLoading: (isLoading: boolean) => void, isLoading: boolean}) => {
 
     const [prompt, setPrompt] = useState('');
+    const {user, chats, setChats, selectedChat, setSelectedChat} = useAppContext();
+    
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>)=>{
+        if(e.key === "Enter" && !e.shiftKey){
+            e.preventDefault();
+            sendPrompt(e);
+        }
+    }
 
+    const sendPrompt = async (e: React.FormEvent<HTMLFormElement> | React.KeyboardEvent<HTMLTextAreaElement>)=>{
+        e.preventDefault();
+        const promptCopy = prompt;
+
+        try {
+            if(!user) return toast.error("Login to send message");
+            if(isLoading) return toast.error('Wait for the previous prompt response');
+
+            setIsLoading(true)
+            setPrompt("")
+
+            const userPrompt = {
+                role: "user",
+                content: prompt,
+                timestamp: Date.now(),
+            }
+
+            // saving user prompt in chats array
+
+            setChats((prevChats: any)=> prevChats.map((chat: any)=> chat._id === selectedChat._id ?
+             {
+                ...chat,
+                messages: [...chat.messages, userPrompt]
+            }: chat
+        ))
+        // saving user prompt in selected chat
+
+        setSelectedChat((prev: any)=> ({
+            ...prev,
+            messages: [...prev.messages, userPrompt]
+        }))
+
+        const {data} = await axios.post('/api/chat/ai', {
+            chatId: selectedChat._id,
+            prompt
+        })
+
+        if(data.success){
+            setChats((prevChats: any)=>prevChats.map((chat: any)=>chat._id === selectedChat._id ? {...chat, messages: [...chat.messages, data.data]} : chat))
+
+            const message = data.data.content;
+            const messageTokens = message.split(" ");
+            let assistantMessage = {
+                role: 'assistant',
+                content: "",
+                timestamp: Date.now(),
+            }
+
+            setSelectedChat((prev: any) => ({
+                ...prev,
+                messages: [...prev.messages, assistantMessage],
+            }))
+
+            for (let i = 0; i < messageTokens.length; i++) {
+               setTimeout(()=>{
+                assistantMessage.content = messageTokens.slice(0, i + 1).join(" ");
+                setSelectedChat((prev: any)=>{
+                    const updatedMessages = [
+                        ...prev.messages.slice(0, -1),
+                        assistantMessage
+                    ]
+                    return {...prev, messages: updatedMessages}
+                })
+               }, i * 100)
+                
+            }
+        }else{
+            toast.error(data.message);
+            setPrompt(promptCopy);
+        }
+
+        } catch (error) {
+            toast.error(error instanceof Error ? error.message : "An unknown error occurred");
+            setPrompt(promptCopy);
+        } finally {
+            setIsLoading(false);
+        }
+    }
   return (
-    <form 
-     className={`w-full ${0 ? "max-w-3xl" : "max-w-2xl"} bg-[#404045] p-4 rounded-3xl mt-4 transition-all`}>
+    <form onSubmit={sendPrompt}
+     className={`w-full ${selectedChat?.messages.length > 0 ? "max-w-3xl" : "max-w-2xl"} bg-[#404045] p-4 rounded-3xl mt-4 transition-all`}>
         <textarea
+        onKeyDown={handleKeyDown}
         className='outline-none w-full resize-none overflow-hidden break-words bg-transparent'
         rows={2}
         placeholder='Message DeepSeek' required 
--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useAuth, useUser } from "@clerk/nextjs";
+import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext<any>(null);
 
@@ -10,9 +12,71 @@ export const useAppContext = ()=>{
 
 export const AppContextProvider = ({children}: {children: React.ReactNode})=>{
     const {user} = useUser()
+    const {getToken} = useAuth()
+
+    const [chats, setChats] = useState([]);
+    const [selectedChat, setSelectedChat] = useState(null);
+
+    const createNewChat = async ()=>{
+        try {
+            if(!user) return null;
+
+            const token = await getToken();
+
+            await axios.post('/api/chat/create', {}, {headers:{
+                Authorization: `Bearer ${token}`
+            }})
+
+            fetchUsersChats();
+        } catch (error) {
+            toast.error(error instanceof Error ? error.message : "An unknown error occurred")
+        }
+    }
+
+    const fetchUsersChats = async ()=>{
+        try {
+            const token = await getToken();
+            const {data} = await axios.get('/api/chat/get', {headers:{
+                Authorization: `Bearer ${token}`
+            }})
+            if(data.success){
+                console.log(data.data);
+                setChats(data.data)
+
+                 // If the user has no chats, create one
+                 if(data.data.length === 0){
+                    await createNewChat();
+                    return fetchUsersChats();
+                 }else{
+                    // sort chats by updated date
+                    data.data.sort((a: any, b: any)=> new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime());
+
+                     // set recently updated chat as selected chat
+                     setSelectedChat(data.data[0]);
+                     console.log(data.data[0]);
+                 }
+            }else{
+                toast.error(data.message as string)
+            }
+        } catch (error) {
+            toast.error(error instanceof Error ? error.message : "An unknown error occurred")
+        }
+    }
+
+ useEffect(()=>{
+    if(user){
+        fetchUsersChats();
+    }
+ }, [user])
 
     const value = {
         user,
+        chats,
+        setChats,
+        selectedChat,
+        setSelectedChat,
+        fetchUsersChats,
+        createNewChat
     }
     
     return <AppContext.Provider value={value}>{children}</AppContext.Provider>
